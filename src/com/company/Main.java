eeeae0e1f792134bc50def257b3dcff753724fe8
VeraMG98 <rusyamg@mail.ru>
2020-11-24 19:42:25 +0300

    bla

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        Shop shop = new Shop("Audi shop", "Bishkek");
+        /*Shop shop = new Shop("Audi shop", "Bishkek");
         Shop shop1 = new Shop("Trains", "Novopokrovka");
         Audi audi = new Audi("Auto",shop, "2020",false);
         audi.printAbilityForType();
@@ -18,6 +22,35 @@ public class Main {
         System.out.println(train.printInfo());
 
         audi.copy(audi1);
-        System.out.println(audi.printInfo());
+        System.out.println(audi.printInfo());*/
+
+
+        ArrayList<Person> persons =  new ArrayList<>();
+        Person person;
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Add person or show list?");
+            String word = scanner.nextLine();
+            if (word.equals("Add")) {
+                System.out.println("Enter person's name, address and age");
+                String name = scanner.nextLine();
+                String address = scanner.nextLine();
+                int age = scanner.nextInt();
+                person = new Person(name, age, address);
+                persons.add(person);
+            } else if (word.equals("Show")) {
+                for (Person person1:persons) {
+                    System.out.println(person1.getName() + " " + person1.getAge() + " " + person1.getAddress());
+                }
+            }
+        }
+    }
+
+    public static void printArr(ArrayList<Person> arrayList){
+        Iterator<Person> iterator = arrayList.iterator();
+        while (iterator.hasNext()){
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
     }
 }
